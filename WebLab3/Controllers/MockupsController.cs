e6f1a1c7a94e045214500b9534e5aff3c46f127b
Denis Pokrovskiy <12den00@gmail.com>
2021-06-16 03:25:53 +0700

    Initial commit

---
+++ WebLab3/Controllers/MockupsController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WebLab3.Models;
+
+namespace WebLab3.Controllers
+{
+    public class MockupsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Questions()
+        {
+            QuestionsModel Model = QuestionsModel.Instance;
+            Model.Reset();
+            Model.Begin();
+            return View(Model);
+        }
+
+        [HttpPost]
+        public IActionResult Questions(QuestionsModel Model, string action)
+        {
+            Model = QuestionsModel.Instance;
+
+            if (Request.Form["useranswer"] != "")
+            {
+                Model.useranswer = Int32.Parse(Request.Form["useranswer"]);
+                Model.Accept();
+
+                if (action == "Next")
+                {
+                    QuestionsModel Modelq = QuestionsModel.Instance;
+                    Modelq.Begin();
+                    return View(Modelq);
+                }
+                return RedirectToAction("Result");
+            }
+            else
+            {
+                QuestionsModel Modelq = QuestionsModel.Instance;
+                return View(Modelq);
+            }
+
+        }
+        public IActionResult Result()
+        {
+            QuestionsModel Model = QuestionsModel.Instance;
+            if (Model.Answers == null)
+                ViewBag.Result = "";
+            else 
+                ViewBag.Result = Model.Answers;
+            ViewData["Total"] = "" + Model.count;
+            ViewData["Correct"] = "" + Model.ranswers;
+            return View();
+        }
+    }
+}
---
+++ WebLab3/Models/QuestionsModel.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebLab3.Models
+{
+    public class QuestionsModel
+    {
+        public double n1;
+        public double n2;
+        public double answer;
+        public string operation;
+        public int count;
+        public int useranswer;
+        public int ranswers;
+        public List<string> Answers;
+        string[] ListOperations = { "+", "-", "*", "/" };
+        public static QuestionsModel Instance = new QuestionsModel();
+
+        public void Begin()
+        {
+            Random rand = new Random();
+            n1 = rand.Next(0, 10);
+            n2 = rand.Next(0, 10);
+            operation = ListOperations[rand.Next(0, 3)];
+            count++;
+        }
+
+        public void Accept()
+        {
+            if (operation == "+")
+            {
+                answer = n1 + n2;
+                if (answer == useranswer)
+                    ranswers++;
+                Answers.Add("" + n1 + " + " + n2 + " = " + useranswer);
+            }
+            if (operation == "-")
+            {
+                answer = n1 - n2;
+                if (answer == useranswer)
+                    ranswers++;
+                Answers.Add("" + n1 + " - " + n2 + " = " + useranswer);
+            }
+            if (operation == "*")
+            {
+                answer = n1 * n2;
+                if (answer == useranswer)
+                    ranswers++;
+                Answers.Add("" + n1 + " * " + n2 + " = " + useranswer);
+            }
+            if (operation == "/")
+            {
+                if (n2 != 0)
+                {
+                    answer = n1 / n2;
+                    if (answer == useranswer)
+                        ranswers++;
+                    Answers.Add("" + n1 + " / " + n2 + " = " + useranswer);
+                }
+            }
+        }
+
+        public void Reset()
+        {
+            count = 0;
+            ranswers = 0;
+            Answers = new List<string>();
+        }
+    }
+}
